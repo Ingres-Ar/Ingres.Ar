d157b9687f12395a4080f25515bc124b579c184a
Leo_KING44 <leandrozanoni77@gmail.com>
2020-09-13 19:04:54 -0300

    actualizacion de rama con carpetas nuevas de apps , static de cada app (afuera) y templates en otra carpeta (afuera) , templates de cada app (dentro de app)

--- ingres.ar/project/ingresar/settings/base.py
+++ ingres.ar/project/ingresar/settings/base.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'ingresar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+""" 
 DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -88,11 +89,13 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
-#    }
+    } """
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+  }
 }
 
 
