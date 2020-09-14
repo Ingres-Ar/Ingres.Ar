2eecf431cc9d4e99c1326ce82ea34d2b3b0621ad
Leo_KING44 <leandrozanoni77@gmail.com>
2020-09-14 14:35:14 -0300

    incorporacion de cambios a biblioteca , cada app con estilos propios, cambios a registro, separacion por bloques heredados de base.html

--- ingres.ar/project/ingresar/settings/base.py
+++ ingres.ar/project/ingresar/settings/base.py
@@ -89,12 +89,14 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
-   }
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
-#    }
-}
+   } """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}  
 
 
 # Password validation
