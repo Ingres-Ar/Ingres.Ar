6415649c89b65fc956ec60d4e282c614baf7c3c9
MaxiSaito <maxisaito@gmail.com>
2020-09-18 12:45:52 -0300

    home new carousel

--- ingres.ar/project/ingresar/settings/base.py
+++ ingres.ar/project/ingresar/settings/base.py
@@ -89,8 +89,7 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
-<<<<<<< HEAD
-   } """
+} """
 
 DATABASES = {
     'default': {
@@ -98,14 +97,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }  
-=======
-}
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
-#    }
-}
->>>>>>> 93bd37d61ab76871ce9a1614ce3bb3dc74437242
 
 
 # Password validation
