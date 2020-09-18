aa106dce157b9864474a5922b4f7c40d74cef5c1
LucasEscribe <lfd.meneses@gmail.com>
2020-09-18 20:00:30 -0300

    copiando app users de portfolioInfoDev utilizando app usuarios de Ingres.Ar

--- ingres.ar/project/ingresar/settings/base.py
+++ ingres.ar/project/ingresar/settings/base.py
@@ -139,4 +139,4 @@ STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
-print(MEDIA_ROOT)
+#print(MEDIA_ROOT)
