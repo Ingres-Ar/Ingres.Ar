492f669d431da4e9724ecf6b1406ae3af14673e3
JuanLedesma <juanjoseledesma90@gmail.com>
2020-09-22 11:39:03 -0300

    agregado: búsqueda funcionando, hay que modificar modelos para subir datos reales

--- ingres.ar/project/apps/Establecimientos/urls.py
+++ ingres.ar/project/apps/Establecimientos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.establecimientos, name='establecimientos'),
+    path('buscar_colegios', views.filtro, name='buscar_colegios'),
+    
 ]
--- ingres.ar/project/apps/Establecimientos/views.py
+++ ingres.ar/project/apps/Establecimientos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 
@@ -6,3 +6,10 @@ def establecimientos(request):
     colegios = Establecimiento.objects.all()
     context = {'colegios_clave': colegios }
     return render(request,'establecimientos.html',context)
+
+def filtro(request):
+    if request.method == 'POST':
+        query = Establecimiento.objects.filter(nombre__icontains=request.POST['busqueda'])
+        resultado = {'resultado': query}
+        return render(request,'establecimientos.html',resultado)
+    return redirect('establecimientos')
