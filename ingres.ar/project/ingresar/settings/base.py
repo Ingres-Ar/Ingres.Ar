b5dce701e8492004a37061a35b283de328b52b9b
JuanLedesma <juanjoseledesma90@gmail.com>
2020-09-21 22:58:09 -0300

    primer commit de expermentos. Se configura settings para que trabaje con postgres

--- ingres.ar/project/ingresar/settings/base.py
+++ ingres.ar/project/ingresar/settings/base.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'ingresar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" 
 DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ingresar',
-        'USER': 'postgres',
+        'USER': 'Admin',
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
-} """
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
-}  
+} 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+#     }
+# }  
 
 
 # Password validation
