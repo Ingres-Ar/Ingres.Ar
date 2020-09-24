9c963a3a735cf0b3c53a1dea67b72472317794bf
JuanLedesma <juanjoseledesma90@gmail.com>
2020-09-24 10:41:43 -0300

    agregado paginacion

--- ingres.ar/project/apps/Establecimientos/urls.py
+++ ingres.ar/project/apps/Establecimientos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.establecimientos, name='establecimientos'),
     path('buscar_colegios', views.filtro, name='buscar_colegios'),
+    path('listar_colegios', views.listing, name='listar_colegios'),
     
 ]
--- ingres.ar/project/apps/Establecimientos/views.py
+++ ingres.ar/project/apps/Establecimientos/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def establecimientos(request):
-    colegios = Establecimiento.objects.all()
-    context = {'colegios_clave': colegios }
-    return render(request,'establecimientos.html',context)
+    return render(request,'establecimientos.html')
 
 def filtro(request):
     if request.method == 'POST':
@@ -15,4 +15,20 @@ def filtro(request):
             return render(request,'establecimientos.html',resultado)
         else:
            return render(request,'establecimientos.html',{'error_busqueda':'Debes ingresar una palabra clave para buscar'}) 
-    return redirect('establecimientos')
+    #  return redirect('establecimientos')
+    lista = Establecimiento.objects.all()
+    paginator = Paginator(lista, 10) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'establecimientos.html', {'lista_colegios': page_obj})
+
+
+def listing(request):
+    
+    lista = Establecimiento.objects.all()
+    paginator = Paginator(lista, 10) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'establecimientos.html', {'lista_colegios': page_obj})
