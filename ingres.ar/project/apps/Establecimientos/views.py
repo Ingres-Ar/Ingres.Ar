aeb2b5a22ac64ad4c7fe1793dc59a805664f2725
Leo_KING44 <leandrozanoni77@gmail.com>
2020-09-24 20:07:05 -0300

    cambios a paginacion y estilos

--- ingres.ar/project/apps/Establecimientos/views.py
+++ ingres.ar/project/apps/Establecimientos/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from .models import *
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def establecimientos(request):
-    colegios = Establecimiento.objects.all()
-    context = {'colegios_clave': colegios }
-    return render(request,'establecimientos.html',context)
+    if request.method != 'POST':
+
+        lista = Establecimiento.objects.all()
+        paginator = Paginator(lista, 5) # Show 25 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'establecimientos.html', {'lista_colegios': page_obj})
+    return redirect(request, 'establecimientos')
+
 
 def filtro(request):
     if request.method == 'POST':
@@ -15,12 +25,24 @@ def filtro(request):
             return render(request,'establecimientos.html',resultado)
         else:
            return render(request,'establecimientos.html',{'error_busqueda':'Debes ingresar una palabra clave para buscar'}) 
-    return redirect('establecimientos')
+    #  return redirect('establecimientos')
+    
+
+def listing(request):
+    
+    lista = Establecimiento.objects.all()
+    paginator = Paginator(lista, 10) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'establecimientos.html', {'lista_colegios': page_obj})
+
+def info_establecimientos(request):
+    pass
 
 
-""" def busqueda(request):
-    if request.method == 'GET':
-        busqueda = request.GET['Establecimiento.objects.filter()']
-        context = {'colegios_clave': colegios }
-        return render(request, 'establecimientos.html',{'busqueda':'busqueda'})
-            # return redirect('home') """
+
+
+
+
+
